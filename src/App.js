cdd2c73defa6d27bb0d8aa97519693ad11e989aa
wjf <wilson.forero@uan.edu.co>
2019-03-23 12:17:44 -0500

    merge

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="YourApp">
+      <div className="YourApp"> 
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
